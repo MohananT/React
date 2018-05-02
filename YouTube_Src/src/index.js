f106d9fbe9aa127eddac4193ee03e84191ea3ec2
MohananT <mohan.stanky@gmail.com>
2018-05-03 00:14:57 +0530

    Video_Specific_Detail added

--- YouTube_Src/src/index.js
+++ YouTube_Src/src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar'
 import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
 
 const API_KEY = "AIzaSyBbbZ3OhOnr9Iy0LSKdJyz1JN8gucTMOUg";
 
@@ -27,6 +28,7 @@ class App extends React.Component{
     return(
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
         <VideoList videos={this.state.videos} />
       </div>
     );
